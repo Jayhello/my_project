dcb472ffaf2c46e0d4e5dc86e8af66126d5ab695
Jayhello <haluoluo@qq.com>
2020-08-13 19:34:25 +0800

    Create demo_yaml.cpp

---
+++ demo/demo_common/demo_yaml.cpp
@@ -0,0 +1,58 @@
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sstream>
+#include <boost/algorithm/string.hpp>
+
+std::string yaml_path = "/mnt/e/c_code/cmake_tutorial1-master/demo_protobuf/test_yaml.yaml";
+using std::cout;
+using std::endl;
+
+int GetNode_(const YAML::Node& root, YAML::Node& sub){
+    sub = root;
+    return 0;
+}
+
+template<typename... Args>
+int GetNode_(const YAML::Node& root, YAML::Node& sub, const std::string& key, const Args&...args){
+    return GetNode_(root[key], sub, args...);
+}
+
+template<typename T, typename... Args>
+int GetValue(T& value, const Args& ... args){
+    YAML::Node root = YAML::LoadFile(yaml_path);
+    YAML::Node sub;
+
+    GetNode_(root, sub, args...);
+    value = sub.as<T>();
+
+    return 0;
+}
+
+void test_yaml(){
+    YAML::Node config = YAML::LoadFile(yaml_path);
+
+    const std::string username = config["username"].as<std::string>();
+    const std::string password = config["password"].as<std::string>();
+
+    // name: c++, pwd: xxx
+    printf("\n name: %s, pwd: %s \n", username.c_str(), password.c_str());
+
+    std::vector<std::string> vec_str = config["members"].as<std::vector<std::string>>();
+    cout<<boost::algorithm::join(vec_str, ", ")<<endl; // xy1, xy2
+
+    cout<<config["msg_info"]["id"].as<uint32_t>()<<endl; // 3
+
+    std::string name;
+    GetValue(name, "username");
+    printf("\n name: %s \n", name.c_str()); // name: c++
+
+    std::vector<std::string> members;
+    GetValue(members, "members");
+    printf("members: %s\n", boost::algorithm::join(members, "->").c_str());
+    // members: xy1->xy2
+
+    uint32_t id;
+    GetValue(id, "msg_info", "id");
+    printf("\n id: %u \n", id); // members: xy1->xy2
+}
