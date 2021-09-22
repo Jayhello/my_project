1bf2c75b9e6b9e300e9eaa93e0120bfe8ed9c829
Jayhello <haluoluo@qq.com>
2021-09-22 22:07:27 +0800

    smart ptr

--- demo/demo_common/demo_comm.cpp
+++ demo/demo_common/demo_comm.cpp
@@ -6,14 +6,51 @@
 #include <memory>
 
 void uniq_ptr_demo();
+void uniq_ptr_c();
 
 int main(){
-    uniq_ptr_demo();
+//    uniq_ptr_demo();
+
+    uniq_ptr_c();
 
     return 0;
 }
 
+class Base{
+public:
+    virtual void print(){printf("this is base: %d\n", a_);}
+
+    int a_ = 0;
+};
+
+class Sub: public Base{
+public:
+    virtual void print(){printf("this is sub: %d\n", a_);}
+};
+
+template<typename T>
+class TestDerive{
+public:
+    void invoke(){
+        t.print();
+    }
+
+    T t;
+};
+
+typedef TestDerive<Base> TestDeriveBase;
+typedef TestDerive<Sub> TestDeriveTestDerive;
+
+void test_tem(TestDeriveBase& tb){
+    tb.invoke();
+}
+
 void uniq_ptr_demo(){
+//    TestDeriveTestDerive td;
+//    test_tem(td);  // compile error
+
+
+
     std::unique_ptr<int> pi(new int(3));
     std::cout << *pi << std::endl;
 
@@ -24,4 +61,12 @@ void uniq_ptr_demo(){
     if(pi){
 
     }
+}
+
+typedef std::unique_ptr<Base> BasePtr;
+typedef std::unique_ptr<Sub>  SubPtr;
+
+void uniq_ptr_c(){
+    BasePtr bp = SubPtr(new Sub);
+    bp->print();
 }
